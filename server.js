7f73c6378932c15cd350f5b39d50d69c5c87808a
waynarogo <prusdasha2021@gmail.com>
2026-04-26 21:57:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ const CONFIG = {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', '*');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
+
+app.use(express.json({ type: '*/*' }));
+app.use(express.urlencoded({ extended: true }));
  
 // ===== HMAC-MD5 підпис для Way4Pay =====
 function wfpSign(fields) {
